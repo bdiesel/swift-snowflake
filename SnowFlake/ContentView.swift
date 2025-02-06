c9fb1b55848d765ba466db17453892fcaf2bb0bd
Brian Diesel <bri@ndiesel.net>
2025-02-06 13:38:22 -0500

    Adding welcome view

--- SnowFlake/ContentView.swift
+++ SnowFlake/ContentView.swift
@@ -39,7 +39,7 @@ struct ContentView: View {
             }
             .navigationBarTitle("Resorts")
         } detail: {
-            Text("Detail")
+            WelcomeView()
         }
     }
 }
